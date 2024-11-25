4b2ed1f8b0666a58fac5622a3a3b5dc648503efd
NatashaStojanova <natashastojanova6@gmail.com>
2024-11-25 15:09:08 +0100

    fix: use different animations on AnswerButton component

--- src/components/AnswerButton/index.tsx
+++ src/components/AnswerButton/index.tsx
@@ -27,17 +27,31 @@ export const AnswerButton = ({
   useEffect(() => {
     if (showCorrect && isCorrect) {
       // Animation used to shake the correct answer when it is revealed
-      gsap.fromTo(
-        buttonRef.current,
-        { x: -5 },
-        {
-          x: 5,
-          duration: 0.1,
-          yoyo: true,
-          repeat: 15,
-          ease: "power1.inOut",
-        }
-      );
+      if (isSelected) {
+        gsap.fromTo(
+          buttonRef.current,
+          { scale: 1 },
+          {
+            scale: 1.2,
+            duration: 0.5,
+            yoyo: true,
+            repeat: 1,
+            ease: "power1.inOut",
+          }
+        );
+      } else {
+        gsap.fromTo(
+          buttonRef.current,
+          { x: -5 },
+          {
+            x: 5,
+            duration: 0.1,
+            yoyo: true,
+            repeat: 15,
+            ease: "power1.inOut",
+          }
+        );
+      }
     }
   }, [isSelected, showCorrect, isCorrect, isWrong]);
 
